53ab7658502338a653410db2e0620391bbcaddd0
jesus-mv <jmart586@ucr.edu>
2024-05-23 21:41:45 -0700

    k_fold_cross_validation done

--- project2.py
+++ project2.py
@@ -1,36 +1,65 @@
 import numpy as np 
-import random
 import copy
 
-# make it so that a user can input a file 
+#general issues: really, really slow on large datasets 
+#                small data 19 picks features [6, 9] with
+#                acc of 95% and not [6, 9, 11] with acc 0.946
+
+# todo: make it so that a user can input a file
 base_file_path = 'data/'
 
-data = np.loadtxt(base_file_path + 'CS205_small_Data__1.txt')
-data_subset = data[:, :5]
+data = np.loadtxt(base_file_path + 'CS205_small_Data__19.txt')
+num_of_classes = data.shape[1] - 1
 
-num_of_classes = data_subset.shape[1]
+local_best_feature_set = []
 
-# might need to turn these into arrays but keep set for now
-local_best_feature_set = set()
-
-global_best_feature_set = set()
+global_best_feature_set = []
 global_best_accuracy = 0
 
-#todo...
-def k_fold_cross_validation(data_subset, best_features, test_feature):
-    return random.randint(0, 100)
+# todo... (?)
+def k_fold_cross_validation(data, best_features, test_feature):
+    
+    num_correct = 0
 
-# search done, code evaluation function
-print("Beginning Search.") #indexing is weird 
-for i in range(0, num_of_classes - 1):
+    temp_best_features = copy.deepcopy(best_features)
+    temp_best_features.append(test_feature)
+    temp_best_features.insert(0, 0) 
+    temp_best_features.sort()
+
+    # makes a new dataset with only best_features + test_features 
+    new_data = data[:, temp_best_features]
+    
+
+    for i in range(new_data.shape[0]):
+        data_to_classify = new_data[i, 1:]
+        label_of_data = new_data[i, 0]
+
+        nn_distance = np.inf
+        for j in range(new_data.shape[0]):
+            if i != j:
+                distance = np.sqrt(np.sum((data_to_classify - new_data[j, 1:]) ** 2))
+                if (distance < nn_distance):
+                    nn_distance = distance
+                    nn_label = new_data[j, 0]
+
+        if label_of_data == nn_label:
+            num_correct += 1
+
+    accuracy = num_correct / new_data.shape[0]
+    return accuracy
+    
+# todo: backward elimination
+print("Beginning Search.") 
+for i in range(0, num_of_classes): #for each class... 
     local_best_feature = None
     local_best_accuracy = 0
-    for curr_feature in range(1, num_of_classes): # features 1, 2, 3, ...
+    for curr_feature in range(1, num_of_classes + 1): # features 1, 2, 3, ... 
         if curr_feature not in local_best_feature_set:
-            curr_accuracy = k_fold_cross_validation(data_subset, local_best_feature_set, curr_feature) #? 
+            curr_accuracy = k_fold_cross_validation(data, local_best_feature_set, curr_feature) #? 
             temp = copy.deepcopy(local_best_feature_set)
-            temp.add(curr_feature)
-            print("Using feature(s)", temp, "accuracy is", str(curr_accuracy) + "%")
+            temp.append(curr_feature)
+            temp.sort()
+            print("Using feature(s)", temp, "accuracy is", str(curr_accuracy))
 
             if (curr_accuracy > local_best_accuracy):
                 local_best_accuracy = curr_accuracy
@@ -40,10 +69,10 @@ for i in range(0, num_of_classes - 1):
                 global_best_accuracy = curr_accuracy
                 global_best_feature_set = temp
                 
-    local_best_feature_set.add(local_best_feature)
+    local_best_feature_set.append(local_best_feature)
+    local_best_feature_set.sort()
 
-    if (i != num_of_classes - 2):
-        print("feature set", local_best_feature_set, "was best, accuracy is", str(local_best_accuracy) + "%")
+    if (i != num_of_classes - 1):
+        print("feature set", local_best_feature_set, "was best, accuracy is", str(local_best_accuracy))
 
-print("Finished search!! The best feature subset is", global_best_feature_set, "which has an accuracy of", str(global_best_accuracy) + "%")
- 
+print("Finished search!! The best feature subset is", global_best_feature_set, "which has an accuracy of", str(global_best_accuracy))
